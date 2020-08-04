0185b384922893c37c928edbfa2d84201021ea21
Jose Juan Suarez Elizalde <jojusuarez46@gmail.com>
2020-08-04 18:26:04 -0500

    Trie implementation

---
+++ Data Structures/Trie/trie_cpp_style.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+typedef struct trienode{
+    trienode *children[28];
+    bool is_word;
+    trienode(){
+        is_word=false;
+        for(int i=0;i<28;i++)
+            children[i]=NULL;
+    }
+}*trie;
+void insert(string s,trie root){
+    trie cur=root;
+    for(int i=0;i<s.size();i++){
+        int index=int(s[i]-'a');
+        if(!cur->children[index])
+            cur->children[index] = new trienode();
+        cur=cur->children[index];
+    }
+    cur->is_word=true;
+}
+int isthere(string s,trie root){
+    trie cur=root;
+    for(int i=0;i<s.size();i++){
+        int index=int(s[i]-'a');
+        if(!cur->children[index])
+            return 0; 
+        cur=cur->children[index];
+    }
+    if(cur->is_word)return 1;
+    return 0;
+}
+int main() {
+    int t;
+	cin>>t;
+	while(t--){
+        int n;
+        cin>>n;
+        trie my_trie = new trienode();
+        for(int i=0;i<n;i++){
+            string s;
+            cin>>s;
+            insert(s,my_trie);
+        }   
+        string look;
+        cin>>look;
+        cout<<isthere(look,my_trie)<<endl;
+	}
+	return 0;
+}
+/*
+test case:
+input:
+2
+8
+the a there answer any by bye their
+the
+3
+cey vpj hvf
+gxp
+
+output:
+1
+0
+*/
