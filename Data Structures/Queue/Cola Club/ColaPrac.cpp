165ece01726503626c35597a31ed7df8f5a59e6d
JoseJuanSE <jojusuarez46@gmail.com>
2020-11-30 23:31:55 -0600

    espacios

--- Data Structures/Queue/Cola Club/ColaPrac.cpp
+++ Data Structures/Queue/Cola Club/ColaPrac.cpp
@@ -1,19 +1,31 @@
 #include<bits/stdc++.h>
 #include "Cola.h"
-#define deb cout<<"deb"<<endl; 
+
 using namespace std;
+
 int main(){
     myqueue *x = new myqueue();
+
     x->push(5);
+
     cout<<x->front()<<endl;
+
     x->pop();
-    if(x->empty())cout<<"yes"<<endl;
-    for(int i=0;i<7;i++)x->push(i);
+
+    if(x->empty())
+        cout<<"yes"<<endl;
+
+    for(int i=0;i<7;i++)
+        x->push(i);
+
     while(!x->empty()){
         cout<<x->front()<<endl;
         x->pop();
     }
+    
     x->push(66);
+    
     cout<<x->front()<<endl;
+    
     return 0;
 }
